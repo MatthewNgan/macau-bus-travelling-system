0d7ddcbfa06332c0f087550269dfe036424a2c98
MatthewNgan <58377002+MatthewNgan@users.noreply.github.com>
2020-12-18 22:46:53 +0800

    done

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ Vue.createApp({
       this.routesGenerated = {};
       this.currentlyOpenedIndex = undefined;
       // document.getElementById('home').scrollIntoView();
-      document.body.classList.rem("no-scroll");
+      document.body.classList.remove("no-scroll");
     },
     getRoutes() {
       fetch(`${this.corsProxy}https://bis.dsat.gov.mo:37812/macauweb/getRouteAndCompanyList.html`)
