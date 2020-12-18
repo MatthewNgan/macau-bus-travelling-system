391162483f04ac0ffc8c3da00ade04fcad44a001
MatthewNgan <58377002+MatthewNgan@users.noreply.github.com>
2020-12-18 22:09:42 +0800

    no

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ Vue.createApp({
       this.routesGenerated = {};
       this.currentlyOpenedIndex = undefined;
       // document.getElementById('home').scrollIntoView();
+      document.querySelector('#home').scroll = "yes";
     },
     getRoutes() {
       fetch(`${this.corsProxy}https://bis.dsat.gov.mo:37812/macauweb/getRouteAndCompanyList.html`)
@@ -52,6 +53,7 @@ Vue.createApp({
       this.scroll = true;
       this.busColor = color;
       this.routeChanged();
+      document.querySelector('#home').scroll = "no";
     },
     calculateDistance(lon1,lat1,lon2,lat2){
       const R = 6371e3; // metres
