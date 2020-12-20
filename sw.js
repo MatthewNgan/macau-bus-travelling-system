c52e2e8942af768eefe8ae53039a1fbaaa21e418
MatthewNgan <58377002+MatthewNgan@users.noreply.github.com>
2020-12-21 01:47:55 +0800

    PWA

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 self.addEventListener('install', e => {
     console.log('ServiceWorker has been installed');
 })
+
+self.addEventListener('activate', e => {
+    console.log('ServiceWorker has been activated');
+})
+
+self.addEventListener('fetch', e => {
+    console.log('Fetch event', e)
+})
