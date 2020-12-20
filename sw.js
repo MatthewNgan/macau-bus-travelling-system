16aaf379b1d1ada8c2ed0c5ae4be91eb99a6cfed
MatthewNgan <58377002+MatthewNgan@users.noreply.github.com>
2020-12-21 02:21:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,32 @@
+const staticCacheName = 'site-assets-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/script.js',
+    '/style.css',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css',
+    'https://cors-anywhere.matthewngan.workers.dev/?https://bis.dsat.gov.mo:37812/macauweb/getRouteAndCompanyList.html'
+
+];
+
 self.addEventListener('install', e => {
-    console.log('ServiceWorker has been installed');
-})
+    // console.log('ServiceWorker has been installed');
+    e.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            cache.addAll(assets);
+        })
+    );
+});
 
 self.addEventListener('activate', e => {
-    console.log('ServiceWorker has been activated');
-})
+    // console.log('ServiceWorker has been activated');
+});
 
 self.addEventListener('fetch', e => {
-    console.log('Fetch event', e)
-})
+    // console.log('Fetch event', e)
+    e.respondWith(
+        caches.match(e.request).then(cacheRes => {
+            return cacheRes || fetch(e.request);
+        })
+    )
+});
