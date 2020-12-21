3b569be863f55e64058dd3a65f9909b36127c269
MatthewNgan <58377002+MatthewNgan@users.noreply.github.com>
2020-12-21 10:09:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
 const staticCacheName = 'site-assets-static-1';
-const count = 0;
 const assets = [
     '/',
     '/index.html',
@@ -10,8 +9,7 @@ const assets = [
     'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js',
     'https://unpkg.com/vue@3.0.4/dist/vue.global.js',
-    'https://bus.matthewngan.ga/apple-icon-144x144-dunplab-manifest-25120.png',
-
+    'https://bus.matthewngan.ga/apple-icon-144x144-dunplab-manifest-25120.png'
 ];
 
 self.addEventListener('install', e => {
@@ -39,22 +37,6 @@ self.addEventListener('fetch', e => {
     // console.log('Fetch event', e)
     e.respondWith(
         caches.match(e.request).then(cacheRes => {
-    //         if (e.request.url.includes('dsat.gov.mo')) {
-    //             if (!cacheRes) {
-    //                 return fetch(e.request).then(fetchRes => {
-    //                     return caches.open("site-route-info-"+count).then(cache => {
-    //                         cache.put(e.request.url, fetchRes.clone());
-    //                         return fetchRes;
-    //                     })
-    //                 });
-    //             }
-    //             if (Date.now() > cacheRes.headers.get('date') + 15000) {
-    //                 caches.delete("site-route-info-" + (count-1).toString());
-    //             }
-    //             if (cacheRes == undefined || Date.now() > cacheRes.headers.get('date') + 15000) {
-                    
-    //             }
-    //         }
             return cacheRes || fetch(e.request.url);
         })
     )
