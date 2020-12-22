5ca2796079534d6b82b8875ab6256f36ad12d780
MatthewNgan <58377002+MatthewNgan@users.noreply.github.com>
2020-12-22 12:41:55 +0800

    Added rounded logo to service worker static cache
    #26

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const staticCacheName = 'site-assets-static#25';
+const staticCacheName = 'site-assets-static#26';
 const assets = [
     '/',
     '/index.html',
     '/script.js',
     '/style.css',
     '/logo.png',
+    '/rounded-logo.png',
     '/manifest.json',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js',
