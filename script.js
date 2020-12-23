1697d0e33fe25657447b649305f4dc87614ec30b
MatthewNgan <58377002+MatthewNgan@users.noreply.github.com>
2020-12-23 19:53:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -390,7 +390,7 @@ Vue.createApp({
 
     this.busMap = L.map('bus-map');
     this.busMap.setView([22.17,113.5597966], 12);
-    this.mapLayerGroup = L.layerGroup().addTo(this.busMap);
+    this.mapLayerGroup = L.featureGroup().addTo(this.busMap);
     L.tileLayer(`${this.corsProxy}https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`, {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
